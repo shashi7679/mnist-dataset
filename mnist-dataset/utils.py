e8034579f3eb3475155a3bfe477bb09b216d306d
Shashi7679 <shashikantprasad1111@gmail.com>
2021-10-07 17:09:16 +0530

    Utility Program

--- mnist-dataset/utils.py
+++ mnist-dataset/utils.py
@@ -1,9 +1,13 @@
+from joblib import dump,load
+from numpy.core.getlimits import _fr1
 import sklearn
 from skimage import transform
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import datasets,svm,metrics
 from sklearn.model_selection import train_test_split
+import os
+
 def preprocessing(images,rescale_factor):
     resized_images = []
     for img in images:
@@ -19,4 +23,27 @@ def create_split(data,target,train_size,test_size,val_size):
 def get_acc(model,X,Y):
     pred = model.predict(X)
     acc = metrics.accuracy_score(Y,pred)
-    return acc
+    f1 = metrics.f1_score(y_true=Y,y_pred=pred,average=None)
+    return {"acc": acc, "f1": f1 }
+
+def get_random_acc(y):
+    return max(np.bincount(y))/len(y)
+
+def run_classification_exp(train_X,train_Y,val_X,val_Y,gamma,output_model_path):
+    rand_val_acc = get_random_acc(val_Y)
+
+    model = svm.SVC(gamma = gamma)
+
+    model.fit(train_X,train_Y)
+
+    metrics_valid = get_acc(model= model,X = val_X,Y = val_Y)
+    #print(metrics_valid)
+    if metrics_valid["acc"] < rand_val_acc:
+        print("Skipping for {}".format(gamma))
+        return None
+    #print(output_model_path)
+    output_folder = os.path.dirname(output_model_path)
+    if not os.path.isdir(output_folder):
+        os.mkdir(output_folder)
+    dump(model,output_model_path)
+    return metrics_valid
